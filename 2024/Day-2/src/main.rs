4fee89990f94d590ee9da8d1e04446b79163ece4
Dustdev_DM <public@dustin-dm.de>
2024-12-02 20:52:50 +0100

    result for day 2 star 2

--- 2024/Day-2/src/main.rs
+++ 2024/Day-2/src/main.rs
@@ -6,22 +6,39 @@ fn main() {
         .expect("Should have been able to read the file");
     input = input.trim_start_matches('\u{feff}').to_string();
     let reports = parse_input(input);
-    let save_report_count = reports.iter().filter(|&report| is_report_safe(report)).count();
+    let save_report_count = reports.iter().filter(|&report| {
+        is_report_safe(report)
+    }).count();
 
-    println!("Result 1: {}", save_report_count)
+    println!("Result 1: {}", save_report_count);
+
+    let mut save_report_with_damper_count = 0;
+    for report in reports {
+        let mut result = is_report_safe(&report);
+        if !result {
+            for i in 0..report.len() {
+                let mut reportclone = report.clone();
+                reportclone.remove(i);
+
+                let result_two = is_report_safe(&reportclone);
+                if result_two {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        if result {
+            save_report_with_damper_count += 1;
+        }
+    }
+    println!("Result 2: {}", save_report_with_damper_count);
+
 }
 
 fn parse_input(input: String) -> Vec<Vec<i32>> {
-    let mut reports = Vec::new();
-    for level in input.split("\n") {
-        println!("{level}");
-        let mut parsed_level = Vec::new();
-        for unit in level.split_whitespace() {
-            parsed_level.push(unit.parse().unwrap())
-        }
-        reports.push(parsed_level);
-    }
-    reports
+    input.lines().map(|report| {
+        report.split_whitespace().map(|unit| unit.parse().unwrap()).collect()
+    }).collect()
 }
 
 fn is_report_safe(report: &Vec<i32>) -> bool {
@@ -33,6 +50,5 @@ fn is_report_safe(report: &Vec<i32>) -> bool {
     let ascending = report.windows(2).all(|pair| pair[0] < pair[1]);
     let descending = report.windows(2).all(|pair| pair[0] > pair[1]);
 
-    println!("{0}\nDistance: {distance}\nAscending: {ascending}\nDescending: {descending}\n---", format!("{:?}", report));
     distance & (ascending || descending)
 }
