7c2b07331029a9ecc60f8adc8374da96713530be
Dustdev_DM <public@dustin-dm.de>
2024-12-02 18:49:26 +0100

    result for day 2 star 1

--- 2024/Day-2/src/main.rs
+++ 2024/Day-2/src/main.rs
@@ -2,12 +2,37 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let input = fs::read_to_string(Path::new("./src/input.txt"))
+    let mut input = fs::read_to_string(Path::new("./src/input.txt"))
         .expect("Should have been able to read the file");
-    let input = input.trim_start_matches('\u{feff}').to_string();
+    input = input.trim_start_matches('\u{feff}').to_string();
+    let reports = parse_input(input);
+    let save_report_count = reports.iter().filter(|&report| is_report_safe(report)).count();
 
+    println!("Result 1: {}", save_report_count)
 }
 
-fn parse_input(input: String) {
+fn parse_input(input: String) -> Vec<Vec<i32>> {
+    let mut reports = Vec::new();
+    for level in input.split("\n") {
+        println!("{level}");
+        let mut parsed_level = Vec::new();
+        for unit in level.split_whitespace() {
+            parsed_level.push(unit.parse().unwrap())
+        }
+        reports.push(parsed_level);
+    }
+    reports
+}
 
+fn is_report_safe(report: &Vec<i32>) -> bool {
+    let distance = report.windows(2).all(|pair| {
+        let distance = pair[0].abs_diff(pair[1]);
+        distance <= 3 && distance >= 1
+    });
+
+    let ascending = report.windows(2).all(|pair| pair[0] < pair[1]);
+    let descending = report.windows(2).all(|pair| pair[0] > pair[1]);
+
+    println!("{0}\nDistance: {distance}\nAscending: {ascending}\nDescending: {descending}\n---", format!("{:?}", report));
+    distance & (ascending || descending)
 }
