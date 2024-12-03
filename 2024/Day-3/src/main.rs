3a3dbd8e350ac138ed3d3bf0cf8123ab6ff959b5
Dustdev_DM <public@dustin-dm.de>
2024-12-03 19:04:42 +0100

    result for day 3 star 2

--- 2024/Day-3/src/main.rs
+++ 2024/Day-3/src/main.rs
@@ -1,6 +1,6 @@
+use regex::Regex;
 use std::fs;
 use std::path::Path;
-use regex::Regex;
 
 fn main() {
     let mut input = fs::read_to_string(Path::new("./src/input.txt"))
@@ -9,20 +9,37 @@ fn main() {
 
     let instructions = parse_input(&input);
 
-    let result : i32 = instructions.iter().map(|instruction| instruction[0] * instruction[1]).sum();
+    let result: i32 = instructions
+        .iter()
+        .map(|instruction| instruction[0] * instruction[1])
+        .sum();
 
-    println!("Result 1: {result}")
+    println!("Result: {result}");
 }
 
 fn parse_input(input: &str) -> Vec<Vec<i32>> {
-    let regex = Regex::new(r"mul\(\d{1,4},\d{1,4}\)").unwrap();
-    regex.find_iter(input).map(|instruction| {
-        instruction
-            .as_str()
-            .replace("mul(", "")
-            .replace(")", "")
-            .split(",")
-            .map(|multiplicand| multiplicand.parse().unwrap())
-            .collect()
-    }).collect()
+    let regex = Regex::new(r"(mul\(\d{1,4},\d{1,4}\))|do\(\)|don't\(\)").unwrap();
+    let mut activated = true;
+    regex
+        .find_iter(input)
+        .filter_map(|instruction| match instruction.as_str() {
+            "do()" => {
+                activated = true;
+                None
+            }
+            "don't()" => {
+                activated = false;
+                None
+            }
+            _ if activated => Some(instruction.as_str()),
+            _ => None,
+        })
+        .map(|instruction| { // convert mut(4,3) into Vec<4,3>
+            instruction
+                .split(",")
+                .map(|x| x.chars().filter(|c| c.is_digit(10)).collect())
+                .map(|multiplicand : String| multiplicand.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
