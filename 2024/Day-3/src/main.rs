489c39bbf99e46ab99bcbecd580d5f235f625355
Dustdev_DM <public@dustin-dm.de>
2024-12-03 18:31:36 +0100

    result for day 3 star 1

--- 2024/Day-3/src/main.rs
+++ 2024/Day-3/src/main.rs
@@ -1,12 +1,28 @@
 use std::fs;
 use std::path::Path;
+use regex::Regex;
 
 fn main() {
     let mut input = fs::read_to_string(Path::new("./src/input.txt"))
         .expect("Should have been able to read the file");
     input = input.trim_start_matches('\u{feff}').to_string();
+
+    let instructions = parse_input(&input);
+
+    let result : i32 = instructions.iter().map(|instruction| instruction[0] * instruction[1]).sum();
+
+    println!("Result 1: {result}")
 }
 
-fn parse_input(input: String) {
-
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    let regex = Regex::new(r"mul\(\d{1,4},\d{1,4}\)").unwrap();
+    regex.find_iter(input).map(|instruction| {
+        instruction
+            .as_str()
+            .replace("mul(", "")
+            .replace(")", "")
+            .split(",")
+            .map(|multiplicand| multiplicand.parse().unwrap())
+            .collect()
+    }).collect()
 }
